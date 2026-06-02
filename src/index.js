977842fc3b4f407c5e8c2c62d20cb3b0c59154c6
Kuanyshbek <it13@kulanoil.kz>
2026-06-02 18:20:28 +0500

    chore: publish to npm

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 
-import '../src/styles/tokens.css'
+import './styles/tokens.css'
 
-import Button from './src/components/Button/Button.vue'
-import Input  from './src/components/Input/Input.vue'
-import Badge  from './src/components/Badge/Badge.vue'
-import Card   from './src/components/Card/Card.vue'
-import Modal  from './src/components/Modal/Modal.vue'
-import Loader from './src/components/Loader/Loader.vue'
-import Toast  from './src/components/Toast/Toast.vue'
+import Button from './components/Button.vue'
+import Input  from './components/Input.vue'
+import Badge  from './components/Badge.vue'
+import Card   from './components/Card.vue'
+import Modal  from './components/Modal.vue'
+import Loader from './components/Loader.vue'
+import Toast  from './components/Toast.vue'
 
-export { useToast } from './src/components/Toast/useToast.js'
+export { useToast } from './composable/useToast.js'
 
 const components = { Button, Input, Badge, Card, Modal, Loader, Toast }
 
